1a77c59aad2e9bcc309730d38986510f184b9cb3
zzb66666666x <18767066589@163.com>
2023-11-15 00:00:54 -0600

    Initial commit

---
+++ test/cloud-infra.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CloudInfra from '../lib/cloud-infra-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cloud-infra-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CloudInfra.CloudInfraStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
