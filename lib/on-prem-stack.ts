01880687ac1bd934460ecb1c431a97b9424d716d
zzb66666666x <18767066589@163.com>
2023-12-01 01:42:31 -0600

    change EFS network and security setting for correct mounting

--- lib/on-prem-stack.ts
+++ lib/on-prem-stack.ts
@@ -6,6 +6,7 @@ import { OnPremVpcResources } from './vpc';
 import { EC2NodeResources } from './ec2';
 import { EC2StackProps } from './utils';
 import * as efs from 'aws-cdk-lib/aws-efs';
+import { on } from 'events';
 
 export class OnPremStack extends Stack {
   constructor(scope: Construct, id: string, props: EC2StackProps) {
@@ -32,11 +33,14 @@ export class OnPremStack extends Stack {
 
     const fileSystem = new efs.FileSystem(this, 'OnPremEfsFileSystem', {
       vpc: onpremVpc.vpc,
+      vpcSubnets: { subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS },
+      securityGroup: onpremVpc.onPremSecurityGroup,
       encrypted: true,
       lifecyclePolicy: efs.LifecyclePolicy.AFTER_14_DAYS,
       performanceMode: efs.PerformanceMode.GENERAL_PURPOSE,
       throughputMode: efs.ThroughputMode.BURSTING
     });
+
 
     // create one login side ec2 instance
     const loginNode = new EC2NodeResources(this, 'OnPremLogin', {
@@ -74,7 +78,7 @@ export class OnPremStack extends Stack {
       "yum install -y amazon-efs-utils", 
       "yum install -y nfs-utils", 
       "file_system_id_1=" + fileSystem.fileSystemId, 
-      "efs_mount_point_1=/home", 
+      "efs_mount_point_1=/home/ec2-user", 
       "mkdir -p ${efs_mount_point_1}",
       "if test -f \"/sbin/mount.efs\"; then " +
           "echo \"${file_system_id_1}:/ ${efs_mount_point_1} efs defaults,_netdev 0 0\" >> /etc/fstab; " +
@@ -82,6 +86,8 @@ export class OnPremStack extends Stack {
           "echo \"${file_system_id_1}.efs." + Stack.of(this).region + ".amazonaws.com:/ ${efs_mount_point_1} nfs4 nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport,_netdev 0 0\" >> /etc/fstab; " +
       "fi", 
       "mount -a -t efs,nfs4 defaults",
+      "chown ec2-user:ec2-user /home/ec2-user",
+      "chmod 777 /home/ec2-user",
       "ssh-keygen -t rsa -f /home/ec2-user/.ssh/id_rsa -q -P \"\"",
       "aws ssm put-parameter --name \"/sshkey/onprem/loginNode/id_rsa_pub\" --type \"String\" --value \"$(cat /home/ec2-user/.ssh/id_rsa.pub)\" --overwrite"
     );
@@ -92,7 +98,7 @@ export class OnPremStack extends Stack {
       "yum install -y amazon-efs-utils", 
       "yum install -y nfs-utils", 
       "file_system_id_1=" + fileSystem.fileSystemId, 
-      "efs_mount_point_1=/home", 
+      "efs_mount_point_1=/home/ec2-user",
       "mkdir -p ${efs_mount_point_1}",
       "if test -f \"/sbin/mount.efs\"; then " +
           "echo \"${file_system_id_1}:/ ${efs_mount_point_1} efs defaults,_netdev 0 0\" >> /etc/fstab; " +
@@ -100,6 +106,8 @@ export class OnPremStack extends Stack {
           "echo \"${file_system_id_1}.efs." + Stack.of(this).region + ".amazonaws.com:/ ${efs_mount_point_1} nfs4 nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport,_netdev 0 0\" >> /etc/fstab; " +
       "fi", 
       "mount -a -t efs,nfs4 defaults",
+      "chown ec2-user:ec2-user /home/ec2-user",
+      "chmod 777 /home/ec2-user",
       "mkdir -p /home/ec2-user/.ssh"
     );
 
